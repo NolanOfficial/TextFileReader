0eda2327a2ea3f1c457d7e069bf727748b39eabd
Nolan Fuchs <nolan1235@gmail.com>
2018-10-01 18:58:31 -0700

    Final Commit (v1.0.0)

--- TextFileReader/src/Main.java
+++ TextFileReader/src/Main.java
@@ -1,4 +1,32 @@
+import java.io.*;
+
 
 public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		try {
+		String fileName = "NamesAndNumebrs.txt";
+		FileReader fr = new FileReader(fileName);
+		BufferedReader br = new BufferedReader(fr);
+		
+		String line = br.readLine();
+        while (line != null)
+        {
+          System.out.println(line);
+          line = br.readLine();
+        }
 
+        br.close();
+	}
+	catch (FileNotFoundException exception)
+    {
+       System.out.println ("The file was not found.");
+    }
+    catch (IOException exception)
+    {
+       System.out.println (exception);
+    }
+ }
+	
 }
